bfad5928deea4d65296b82a071f15d8664b3e282
octalparrot <36590822+octalparrot@users.noreply.github.com>
2021-03-02 12:23:41 +0100

    Delete skrypt.sh

--- skrypt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-killall waga_read.sh
-killall dd
-
-/root/waga_read.sh >> /root/waga.tmp
-cat /root/waga.tmp > /root/waga.txt
-rm /root/waga.tmp
-cat /root/waga.txt > /var/www/html/waga.txt
