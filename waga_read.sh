928fa29af127dee3139e2f119c5f45f9698ec1d1
octalparrot <36590822+octalparrot@users.noreply.github.com>
2021-03-27 12:10:13 +0100

    Update waga_read.sh
    Teraz pokazuje także gdy jest 0.00 stabilne

--- waga_read.sh
+++ waga_read.sh
@@ -4,4 +4,4 @@
 #dd if='/dev/ttyUSB0' count=5 status=none | sort | tail -n 2 | tail -n 1 | awk '{print $2}' | sed s/...$//
 
 #Nowa waga
-dd if='/dev/ttyUSB0' count=5 status=none | grep B0 | head -n 1 | tail -n1 | sed -e 's/B0//g' | awk '{print $1}'
+dd if='/dev/ttyUSB1' count=5 status=none | grep -E 'B0|F0' | head -n 1 | tail -n1 | sed -e 's/B0//g' | sed -e 's/F0//g' | awk '{print $1}'
